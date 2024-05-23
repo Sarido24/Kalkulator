293f3671574557fc4e84dfbba92bdcfa5ddf96e3
SARIDO_SIMANULLANG <sarido24@gmail.com>
2024-05-23 22:30:53 +0700

    Making and linking script.js with the index.html and try to make some logic there, also make sure all the number button works

---
+++ script.js
@@ -0,0 +1,39 @@
+let monitor = document.getElementById("monitor");
+function addNum1(){
+    if(monitor.innerHTML)
+    monitor.innerHTML += 1
+}
+function addNum2(){
+    monitor.innerHTML += 2
+}
+function addNum3(){
+    monitor.innerHTML += 3
+}
+function addNum4(){
+    monitor.innerHTML += 4
+}
+function addNum5(){
+    monitor.innerHTML += 5
+}
+function addNum6(){
+    monitor.innerHTML += 6
+}
+function addNum7(){
+    monitor.innerHTML += 7
+}
+function addNum8(){
+    monitor.innerHTML += 8
+}
+function addNum9(){
+    monitor.innerHTML += 9
+}
+function addNum0(){
+    monitor.innerHTML += 0
+}
+function addDecimal(){
+    monitor.innerHTML += "."
+}
+
+function allClear() {
+        monitor.innerHTML = 0
+}
