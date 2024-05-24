f702c443e6e2e81fc8a3c13d7755f8a03051cf14
SARIDO_SIMANULLANG <sarido24@gmail.com>
2024-05-24 20:59:01 +0700

    Finish the basic calculate, still can't autocalculate when the operator button is pressed the second times after a number

--- script.js
+++ script.js
@@ -1,39 +1,95 @@
 let monitor = document.getElementById("monitor");
 function addNum1(){
-    if(monitor.innerHTML)
-    monitor.innerHTML += 1
+    if(monitor.innerHTML === "0"){
+        monitor.innerText = "1"
+    }else{
+        monitor.innerText += "1"
+    }
 }
 function addNum2(){
-    monitor.innerHTML += 2
+    if(monitor.innerHTML === "0"){
+        monitor.innerText = "2"
+    }else{
+        monitor.innerText += "2"
+    }
 }
 function addNum3(){
-    monitor.innerHTML += 3
+    if(monitor.innerHTML === "0"){
+        monitor.innerText = "3"
+    }else{
+        monitor.innerText += "3"
+    }
 }
 function addNum4(){
-    monitor.innerHTML += 4
+    if(monitor.innerHTML === "0"){
+        monitor.innerText = "4"
+    }else{
+        monitor.innerText += "4"
+    }
 }
 function addNum5(){
-    monitor.innerHTML += 5
+    if(monitor.innerHTML === "0"){
+        monitor.innerText = "5"
+    }else{
+        monitor.innerText += "5"
+    }
 }
 function addNum6(){
-    monitor.innerHTML += 6
+    if(monitor.innerHTML === "0"){
+        monitor.innerText = "6"
+    }else{
+        monitor.innerText += "6"
+    }
 }
 function addNum7(){
-    monitor.innerHTML += 7
+   if(monitor.innerHTML === "0"){
+        monitor.innerText = "7"
+    }else{
+        monitor.innerText += "7"
+    }
 }
 function addNum8(){
-    monitor.innerHTML += 8
+    if(monitor.innerHTML === "0"){
+        monitor.innerText = "8"
+    }else{
+        monitor.innerText += "8"
+    }
 }
 function addNum9(){
-    monitor.innerHTML += 9
+    if(monitor.innerHTML === "0"){
+        monitor.innerText = "9"
+    }else{
+        monitor.innerText += "9"
+    }
 }
 function addNum0(){
-    monitor.innerHTML += 0
+    if(monitor.innerHTML !== "0"){
+        monitor.innerHTML += "0"
+    }else{
+        return
+    }
 }
 function addDecimal(){
     monitor.innerHTML += "."
 }
 
 function allClear() {
-        monitor.innerHTML = 0
+        monitor.innerHTML = "0"
+}
+
+function addPlus(){
+    monitor.innerHTML += "+"
+}
+function addMinus(){
+    monitor.innerHTML += "-"
+}
+function addMultiple(){
+    monitor.innerHTML += "*" 
+}
+function addDivide(){
+    monitor.innerHTML += "/"
+}
+
+function getResult(){
+    monitor.innerHTML = eval(monitor.innerHTML)
 }
